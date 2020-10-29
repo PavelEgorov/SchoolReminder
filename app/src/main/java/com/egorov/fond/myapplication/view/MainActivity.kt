2d5e773970d2d04332f528788a54c28369250ac1
PavelEgorov <jonnycomein@gmail.com>
2020-10-30 01:52:12 +0300

    Добавил Layout.

--- app/src/main/java/com/egorov/fond/myapplication/view/MainActivity.kt
+++ app/src/main/java/com/egorov/fond/myapplication/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.egorov.fond.myapplication
+package com.egorov.fond.myapplication.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.egorov.fond.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
